e91239a6449cb5f32c01fb4eb2d28aefdf64b26b
FoboIX <mills_forrest@yahoo.com>
2023-07-10 18:38:22 -0500

    working on login form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,23 @@ const style = {
   navLeft: `flex p-2 w-full items-center`,
   navButtons: `rounded-lg w-1/2 p-2 m-2 bg-slate-800`,
   modal: `m-2 border h-full flex flex-col items-center place-content-center w-auto`,
-  exit: `w-96 border text-black`,
+  exit: `w-96 text-black flex justify-end`,
+  exitIcon: `rounded-lg h-8 w-8 bg-red-800`,
   modalForm: `flex flex-col items-center place-content-center bg-red-800 w-80 h-48 p-2 m-2 rounded-lg`,
   modalInputFields: `rounded-md p-2 m-2 text-black`,
 };
 
 function NavBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const openModal = () => {
     setIsModalOpen(true);
   };
-  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsModalOpen();
+  };
+
   return (
     <>
       <div className={style.navContainer}>
@@ -34,10 +41,11 @@ function NavBar() {
       <Modal
         className={style.modal}
         isOpen={isModalOpen}
+        ariaHideApp={false}
         onRequestClose={() => setIsModalOpen(false)}
       >
-        <button className={style.exit}>
-          <RiCloseFill size={25} />
+        <button className={style.exit} onClick={closeModal}>
+          <RiCloseFill className={style.exitIcon} size={25} />
         </button>
         <form className={style.modalForm}>
           <input
