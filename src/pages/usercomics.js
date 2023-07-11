fdc080aabd476b6e5a39359ea3949139f1c708dd
FoboIX <mills_forrest@yahoo.com>
2023-07-11 17:49:06 -0500

    addl styling to saved comics page

--- src/pages/usercomics.js
+++ src/pages/usercomics.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import { getSavedComicIds } from "@/utils/localStorage";
 
+const style = {
+  container: `flex flex-wrap border m-2`,
+  comic: `m-2 p-2 w-80 flex flex-col items-center text-center`,
+  img: `rounded-lg scale-75`,
+  button: `rounded-lg bg-red-800 p-2 m-2`,
+};
+
 export default function UserComics() {
   const [comicData, setComicData] = useState([]);
 
@@ -11,15 +18,19 @@ export default function UserComics() {
   console.log(comicData);
 
   return (
-    <div>
-      {comicData.map((comic) => {
-        return (
-          <div key={comic.comicId}>
-            <div>{comic.title}</div>
-            <img src={comic.image} alt={comic.title} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1>Saved Comics</h1>
+      <div className={style.container}>
+        {comicData.map((comic) => {
+          return (
+            <div className={style.comic} key={comic.comicId}>
+              <div>{comic.title}</div>
+              <img className={style.img} src={comic.image} alt={comic.title} />
+              <button className={style.button}>Remove</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
